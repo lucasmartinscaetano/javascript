de4ce15fbcdbf4c972f173d40836ec6c6c25f868
lucasmartinscaetano <108697152+lucasmartinscaetano@users.noreply.github.com>
2022-07-25 13:34:07 -0300

    projeto horas

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('Seja bem vindo!')
+function carregar(){
+    var msg = document.querySelector('div#msg')
+    var imagem = document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = 13
+    msg.innerHTML = `Agora são exatamente ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        imagem.src = 'bomdiateste.png'
+        document.body.style.background = '#fcc537'
+    }
+     else if(hora > 12 && hora < 18 ){
+        imagem.src = 'boatarde.png'  
+        document.body.style.background = '#fda661'        
+     } else {
+        imagem.src = 'boanoite.png'  
+        document.body.style.background = '#1a517a'     
+     }
+    
+
+}
