6558edfe626e0984d7fc6d76ace871203033e4d1
lucasmartinscaetano <108697152+lucasmartinscaetano@users.noreply.github.com>
2022-07-28 03:12:55 -0300

    att

--- ex014/script.js
+++ ex014/script.js
@@ -3,11 +3,29 @@ function clicar(){
     var fim = document.querySelector('#fim')
     var pas = document.querySelector('#pas')
     var res = document.querySelector('#res')
-    var valorinc = Number(inc.value)
-    var valorfim = Number(fim.value)
-    var valorpas = Number(pas.value)
-    for(valorinc; valorfim || valorfim - valorpas; valorpas){
-        res.innerHTML = `${valorinc + valorpas}`
+    if(inc.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
+        window.alert('[ERRO] incluir todos os valores')
+    } else{
+        res.innerHTML = 'Contando: '
+        var i = Number(inc.value)
+        var f = Number(fim.value)
+        var p = Number (pas.value)
+        if(p <= 0){
+            window.alert('Passo inválido, passo vai valer 1')
+            p = 1
+        }
+        if( i < f){
+            for(var c = i; c <= f; c += p){
+                res.innerHTML += `${c} 👉`
+            }
+        } else {
+            for(var c = i; c >=f; c-=p){
+                res.innerHTML += `${c} 👉`
+            }
+        }
+        
     }
+            
+
     
 }
